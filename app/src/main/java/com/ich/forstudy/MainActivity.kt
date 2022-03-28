6c79241cbfe6a83ee3ced2b4f49657d639f168bc
임찬형 <ich0906@naver.com>
2022-03-28 11:39:16 +0900

    commit test 2

--- app/src/main/java/com/ich/forstudy/MainActivity.kt
+++ app/src/main/java/com/ich/forstudy/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             DropdownFilter()
             // For test
-            TODO()
+            TODO("Commit Test")
         }
     }
 }
